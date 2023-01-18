b1dc4d0c28097b2977bc7005834e79a4526d5ff4
Nikita Beliaev <laedamor1993@yandex.ru>
2023-01-18 20:17:54 +0500

    Moved Tasks into a separate class, added scanner close, fixed taskTwo to work according to requirements, changed taskOne and taskThree inputs to work with floating numbers.

---
+++ src/main/java/ru/Astondevs/Tasks.java
@@ -0,0 +1,69 @@
+package ru.Astondevs;
+
+import java.util.Scanner;
+
+public class Tasks {
+    public static Scanner sc = new Scanner(System.in);
+    public static void taskOne() {
+        boolean keep = true;
+        double number;
+        while (keep) {
+            System.out.print("Task 1. Enter a number: ");
+            String input = Main.sc.nextLine();
+            try {
+                number = Double.parseDouble(input);
+            }
+            catch (NumberFormatException exception) {
+                System.out.println("Not a number");
+                continue;
+            }
+            keep = false;
+            if (number > 7)
+                System.out.println("Привет");
+        }
+    }
+
+    public static void taskTwo() {
+        System.out.print("Task 2. Enter a name: ");
+        String name = Main.sc.nextLine();
+        System.out.println(name);
+        if (name.equals("Вячеслав"))
+            System.out.println("Привет, " + name);
+        else
+            System.out.println("Нет такого имени");
+    }
+
+    public static void taskThree() {
+        boolean keep = true;
+        while (keep) {
+            double[] arr;
+            try {
+                System.out.println("Enter an array of numbers, split with space");
+                String array = Main.sc.nextLine();
+                String[] string = array.split(" ");
+                arr = new double[string.length];
+                for (int i = 0; i < string.length; i++) {
+                    arr[i] = Double.parseDouble(string[i]);
+                }
+            }
+            catch (NumberFormatException exception) {
+                System.out.println("Wrong input");
+                continue;
+            }
+            keep = false;
+            System.out.print("Multiples of 3: ");
+            for (double j : arr) {
+                if (j % 3 == 0)
+                    System.out.print(j + " ");
+            }
+        }
+        System.out.println("");
+    }
+
+    public static void taskFour() {
+        System.out.println("Source array: [((())()(())]]");
+        System.out.println("Correct array 1 - adding more brackets to equalize left and right ones: [[((())()(()))]]");
+        System.out.println("Correct array 2 - removing extra brackets to equalize left and right ones: [(())()(())]");
+        System.out.println("Correct array 3 - changing ] for ) to equalize brackets: [((())()(()))]");
+    }
+}
